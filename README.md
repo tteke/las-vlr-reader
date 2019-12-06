7f32f20b6d07a6734287bb6fa6b70656e358a15e
Turker Teke <trkrtk@gmail.com>
2019-12-06 14:33:48 +0300

    converted into a module

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 Read Variable Length Records (VLR) of a LAS formatted point cloud. VLR is often used to store projection data. Usage: 
-```bash
-node index.js pointcloud.las
+In browser
+```javascript
+// Callback from a <input type="file" onchange="onChange(event)">
+function onChange(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+        const arraybuffer = e.target.result;
+        const vlrEntries = window.readLASVLR(arraybuffer);
+
+        console.log(vlrEntries);
+    }; 
+    reader.readAsArrayBuffer(file); 
+}
+```
+
+In nodejs
+```javascript
+const { readLASVLR } = require(/path/to/repo/index.js');
+
+const filePath = '/path/to/las/file.las';
+
+const arraybuffer = fs.readFileSync(filePath);
+
+const vlrEntries = readLASVLR(arraybuffer);
+
+console.log(vlrEntries);
 ```
--- index.js
+++ index.js
@@ -1,107 +1,104 @@
-const fs = require('fs');
-const path = require('path');
+(function (module) {
+    function readLASVLR(arraybuffer) {
+        const signature = readAsString(arraybuffer, 0, 4);
 
-const filepath = path.resolve(process.cwd, process.argv[2]);
-
-console.log(`reading ${filepath}`);
-
-const arraybuffer = fs.readFileSync(filepath);
-
-const signature = readAsString(arraybuffer, 0, 4);
-
-if (signature !== 'LASF') {
-    console.error('not a proper las file');
-    process.exit(1);
-}
-
-// using spec https://www.asprs.org/wp-content/uploads/2010/12/LAS_1_4_r13.pdf
-const versionMajor = arraybuffer.slice(4 + 2 + 2 + 4 + 2 + 2 + 8, 4 + 2 + 2 + 4 + 2 + 2 + 8 + 1)[0];
-const versionMinor = arraybuffer.slice(4 + 2 + 2 + 4 + 2 + 2 + 8 + 1, 4 + 2 + 2 + 4 + 2 + 2 + 8 + 1 + 1)[0];
-console.log(`Las version is ${versionMajor}.${versionMinor}`)
-// headerSize is at 94
-// point data offset is at 96
-const headerSize = readAsTypedArray(arraybuffer, Uint16Array, 94);
-const vlrCount = readAsTypedArray(arraybuffer, Uint16Array, 100);
-
-let currentOffset = headerSize;
-const vlrEntries = [];
-for (let i = 0; i < vlrCount; i++) {
-    // parsing vlr header
-    currentOffset = currentOffset + 2; // reserved for unsigned short
-    const userId = readAsString(arraybuffer, currentOffset, 16); // char[16]
-    currentOffset += 16;
-    const recordId = readAsTypedArray(arraybuffer, Uint16Array, currentOffset); // unsigned short
-    currentOffset += 2;
-    const recordLength = readAsTypedArray(arraybuffer, Uint16Array, currentOffset); // unsigned short
-    currentOffset += 2;
-    const description = readAsString(arraybuffer, currentOffset, 32); // char[32]
-    currentOffset += 32;
-    let data;
-    if (description.includes('GeoKeyDirectoryTag')) {
-        const geoKeyDirectory = {};
-        geoKeyDirectory.wKeyDirectoryVersion = readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
-        currentOffset += 2;
-        geoKeyDirectory.wKeyRevision = readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
-        currentOffset += 2;
-        geoKeyDirectory.wKeyMinorRevision = readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
-        currentOffset += 2;
-        geoKeyDirectory.wKeyNumberOfKeys = readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
-        currentOffset += 2;
-
-        for (let j = 0; j < geoKeyDirectory.wKeyNumberOfKeys; j++) {
-            if (!geoKeyDirectory.keyEntries) {
-                geoKeyDirectory.keyEntries = [];
-            }
-            const keyEntry = {};
-            keyEntry.wKeyId = readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
-            currentOffset += 2;
-            keyEntry.wTIFFTagLocation= readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
-            currentOffset += 2;
-            keyEntry.wCount = readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
-            currentOffset += 2;
-            keyEntry.wValue_Offset = readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
-            currentOffset += 2;
-
-            geoKeyDirectory.keyEntries.push(keyEntry);
+        if (signature !== 'LASF') {
+            console.error('not a proper las file');
+            process.exit(1);
         }
 
-        data = geoKeyDirectory;
-    } else {
-        data = readAsString(arraybuffer, currentOffset,  recordLength); 
-        currentOffset += recordLength;
+        // using spec https://www.asprs.org/wp-content/uploads/2010/12/LAS_1_4_r13.pdf
+        const versionMajor = arraybuffer.slice(4 + 2 + 2 + 4 + 2 + 2 + 8, 4 + 2 + 2 + 4 + 2 + 2 + 8 + 1)[0];
+        const versionMinor = arraybuffer.slice(4 + 2 + 2 + 4 + 2 + 2 + 8 + 1, 4 + 2 + 2 + 4 + 2 + 2 + 8 + 1 + 1)[0];
+        console.log(`Las version is ${versionMajor}.${versionMinor}`)
+        // headerSize is at 94
+        // point data offset is at 96
+        const headerSize = readAsTypedArray(arraybuffer, Uint16Array, 94);
+        const vlrCount = readAsTypedArray(arraybuffer, Uint16Array, 100);
+
+        let currentOffset = headerSize;
+        const vlrEntries = [];
+        for (let i = 0; i < vlrCount; i++) {
+            // parsing vlr header
+            currentOffset = currentOffset + 2; // reserved for unsigned short
+            const userId = readAsString(arraybuffer, currentOffset, 16); // char[16]
+            currentOffset += 16;
+            const recordId = readAsTypedArray(arraybuffer, Uint16Array, currentOffset); // unsigned short
+            currentOffset += 2;
+            const recordLength = readAsTypedArray(arraybuffer, Uint16Array, currentOffset); // unsigned short
+            currentOffset += 2;
+            const description = readAsString(arraybuffer, currentOffset, 32); // char[32]
+            currentOffset += 32;
+            let data;
+            if (description.includes('GeoKeyDirectoryTag')) {
+                const geoKeyDirectory = {};
+                geoKeyDirectory.wKeyDirectoryVersion = readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
+                currentOffset += 2;
+                geoKeyDirectory.wKeyRevision = readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
+                currentOffset += 2;
+                geoKeyDirectory.wKeyMinorRevision = readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
+                currentOffset += 2;
+                geoKeyDirectory.wKeyNumberOfKeys = readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
+                currentOffset += 2;
+
+                for (let j = 0; j < geoKeyDirectory.wKeyNumberOfKeys; j++) {
+                    if (!geoKeyDirectory.keyEntries) {
+                        geoKeyDirectory.keyEntries = [];
+                    }
+                    const keyEntry = {};
+                    keyEntry.wKeyId = readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
+                    currentOffset += 2;
+                    keyEntry.wTIFFTagLocation= readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
+                    currentOffset += 2;
+                    keyEntry.wCount = readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
+                    currentOffset += 2;
+                    keyEntry.wValue_Offset = readAsTypedArray(arraybuffer, Uint16Array, currentOffset);
+                    currentOffset += 2;
+
+                    geoKeyDirectory.keyEntries.push(keyEntry);
+                }
+
+                data = geoKeyDirectory;
+            } else {
+                data = readAsString(arraybuffer, currentOffset,  recordLength); 
+                currentOffset += recordLength;
+            }
+
+            vlrEntries.push({
+                userId,
+                recordId,
+                recordLength,
+                description,
+                data
+            });
+        }
+
+        return vlrEntries;
     }
 
-    vlrEntries.push({
-        userId,
-        recordId,
-        recordLength,
-        description,
-        data
-    });
-}
+    function readAsTypedArray (buf, Type, offset) {
+        let sub = buf.slice(offset, offset + Type.BYTES_PER_ELEMENT);
 
-console.log(JSON.stringify(vlrEntries, "\"", 2));
+        let r = new Type(sub);
 
-function readAsTypedArray (buf, Type, offset) {
-	let sub = buf.slice(offset, offset + Type.BYTES_PER_ELEMENT);
+        return r.reduce((result, byte, index) => {
+            return result + Math.pow(255, index) * byte;    
+        }, 0);
+    };
 
-	let r = new Type(sub);
+    function readAsString(buf, offset, length) {
+        const slice = buf.slice(offset, offset + length);
+        const result = Array.from(slice).map(e => {
+            const char = String.fromCharCode(e);
+            if (char === '\u0000') {
+                return '';
+            }
 
-    return r.reduce((result, byte, index) => {
-        return result + Math.pow(255, index) * byte;    
-    }, 0);
-};
+            return char;
+        }).join('');
 
-function readAsString(buf, offset, length) {
-    const slice = buf.slice(offset, offset + length);
-    const result = Array.from(slice).map(e => {
-        const char = String.fromCharCode(e);
-        if (char === '\u0000') {
-            return '';
-        }
+        return result;
+    };
 
-        return char;
-    }).join('');
-
-    return result;
-};
+    module.readLASVLR = readLASVLR;
+})(typeof module === 'undefined' ? this : module.exports);
